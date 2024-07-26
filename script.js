1169091f3db844bee92fa70af3b4c2df7969b015
Michael Dyer <Dyerm636@gmail.com>
2024-07-25 20:34:29 -0500

    ..

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ deck.shuffle();
 
 var playerCards = document.getElementById("player cards");
 
-/* This will display deck and show all cards
-for (let i = 0;i<deck.cards.length;i++){
+ //This will display deck and show all cards
+function test() {
+    var playerCards = document.getElementById("player cards");
+
+    for (let i = 0;i<deck.cards.length;i++){
     let img = document.createElement("img");
 
     img.src = deck.cards[i].imgName;
@@ -19,5 +22,7 @@ for (let i = 0;i<deck.cards.length;i++){
     playerCards.appendChild(img);
     
 
+    }
+}
 
-}*/
+export default test();
